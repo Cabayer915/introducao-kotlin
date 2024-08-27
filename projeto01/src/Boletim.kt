59ae71ea5789b3dd7fee6ccc09934d86734501b3
Cabayer915 <cauaribeirob915@gmail.com>
2024-08-27 02:36:37 -0300

    Subindo Ex de Boletim em KT

--- projeto01/src/Boletim.kt
+++ projeto01/src/Boletim.kt
@@ -1,2 +1,29 @@
-class Boletim(var estudante: String?, var nota1: Double?, var nota2: Double?) {
+class Boletim(var estudante: String,
+              var nota1: Double,
+              var nota2: Double)
+{
+    init {
+        if (nota1 !in 0.0..10.0) {
+            println("Nota inválida: $nota1. Deve estar entre 0.0 e 10.0.")
+            nota1 = 0.0
+        }
+
+        if (nota2 !in 0.0..10.0) {
+            println("Nota inválida: $nota2. Deve estar entre 0.0 e 10.0.")
+            nota2 = 0.0
+        }
+    }
+
+    fun calcularMedia(): Double {
+        return (nota1 + nota2) / 2
+    }
+
+    fun obterResultado(): String {
+        val media = calcularMedia()
+        return when {
+            media < 5 -> "Reprovação"
+            media < 6 -> "Recuperação"
+            else -> "Aprovação"
+        }
+    }
 }
--- projeto01/src/SistemaEscolar.kt
+++ projeto01/src/SistemaEscolar.kt
@@ -1,9 +1,36 @@
 import java.util.Scanner
 
-fun main(){
-    val leitor = Scanner (System.`in`)
+fun main() {
+    val leitor = Scanner(System.`in`)
 
-    leitor = Scann
+    println("Digite a quantidade de boletins a serem cadastrados:")
+    val quantidade = leitor.nextInt()
+    leitor.nextLine()
 
-    while ()
+    val boletins = mutableListOf<Boletim>()
+
+    for (i in 1..quantidade) {
+        println("Digite o nome do estudante:")
+        val estudante = leitor.nextLine()
+
+        println("Digite a primeira nota:")
+        val nota1 = leitor.nextDouble()
+
+        println("Digite a segunda nota:")
+        val nota2 = leitor.nextDouble()
+        leitor.nextLine()
+
+        val boletim = Boletim(estudante, nota1, nota2)
+        boletins.add(boletim)
+    }
+
+    println("\nRelatório de Boletins:")
+
+    for (boletim in boletins) {
+        if (boletim.estudante.isNotEmpty()) {
+            println("Estudante: ${boletim.estudante}")
+            println("Média: %.2f".format(boletim.calcularMedia()))
+            println("Resultado: ${boletim.obterResultado()}\n")
+        }
+    }
 }
